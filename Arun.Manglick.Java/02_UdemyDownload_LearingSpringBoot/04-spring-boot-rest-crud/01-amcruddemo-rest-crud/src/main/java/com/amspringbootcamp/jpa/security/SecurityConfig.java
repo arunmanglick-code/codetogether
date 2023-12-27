3ddbfb3781a27555e5a711cd02c01459514f7b2d
arunmanglick-code <arunmanglick@gmail.com>
2023-12-26 21:10:45 -0500

    Added Declarative Security  @Configuration

--- Arun.Manglick.Java/02_UdemyDownload_LearingSpringBoot/04-spring-boot-rest-crud/01-amcruddemo-rest-crud/src/main/java/com/amspringbootcamp/jpa/security/SecurityConfig.java
+++ Arun.Manglick.Java/02_UdemyDownload_LearingSpringBoot/04-spring-boot-rest-crud/01-amcruddemo-rest-crud/src/main/java/com/amspringbootcamp/jpa/security/SecurityConfig.java
@@ -8,35 +8,44 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public InMemoryUserDetailsManager userDetailConfiguration(){
-        // {noop} Let’s Spring Security know the passwords are stored as plain text
-
-        UserDetails user1= User.builder()
-                .username("user1")
-                .password("{noop}tester1")
-                .roles("EMPLOYEE")
-                .build();
-
-        UserDetails user2= User.builder()
-                .username("user2")
-                .password("{noop}tester2")
-                .roles("EMPLOYEE","MANAGER")
-                .build();
-
-        UserDetails user3= User.builder()
-                .username("user3")
-                .password("{noop}tester3")
-                .roles("EMPLOYEE","MANAGER","ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(user1,user2,user3);
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
     }
+
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailConfiguration() {
+//        // {noop} Let’s Spring Security know the passwords are stored as plain text
+//
+//        UserDetails user1 = User.builder()
+//                .username("user1")
+//                .password("{noop}tester1")
+//                .roles("EMPLOYEE")
+//                .build();
+//
+//        UserDetails user2 = User.builder()
+//                .username("user2")
+//                .password("{noop}tester2")
+//                .roles("EMPLOYEE", "MANAGER")
+//                .build();
+//
+//        UserDetails user3 = User.builder()
+//                .username("user3")
+//                .password("{noop}tester3")
+//                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(user1, user2, user3);
+//    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
